9f28441e355c6d4adb7f11e294f27a325c9fca3c
abcarso <lainalaire@gmail.com>
2024-11-14 15:58:46 -0500

    Flower prefab editing
    Added water flower prefab. Changed flowerConfig

--- Assets/scripts/objs/FlowerConfig.cs
+++ Assets/scripts/objs/FlowerConfig.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class FlowerConfig : ScriptableObject
 {
     public string flowerType;
-    public int growthStep;
-    public float growthRate;
-    public bool canGrowYearRound;
-    public bool canGrowWinter;
-    public bool canGrowSummer;
-    public bool canGrowFall;
-    public bool canGrowSpring;
-    public bool needWater;
-    public bool needSun;
     public int maxGrowthStage;
+    public float growthRate;
+    public bool diesIfWinter;
+    public int cost;
 }
