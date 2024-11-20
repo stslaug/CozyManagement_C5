74ecf8d504463f00a3cb23443dd17cefd4f68f67
abcarso <lainalaire@gmail.com>
2024-11-20 15:41:30 -0500

    Update inventoryManagement.cs

--- Assets/scripts/gameState/inventoryManagement.cs
+++ Assets/scripts/gameState/inventoryManagement.cs
@@ -1,93 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-
-//Manages flower types, and update counts
-public class inventoryManagement : MonoBehaviour
+public class InventoryManagement : MonoBehaviour
 {
-    public static GameManager gameManager;
-    
-    public TextMeshProUGUI FireFlowerText;
-    public TextMeshProUGUI WaterFlowerText;
-    public TextMeshProUGUI WindFlowerText;
-    
-    public enum ItemType {
-        FireFlower,
-        WaterFlower,
-        WindFlower
-    }
-
-    [System.Serializable]
-    public class Item {
-        public ItemType itemType;
-        public Sprite itemSprite;
-        public int count;
-        public TextMeshProUGUI itemCountText;
-    }
-
+    private GameManager gameManager;
     // Start is called before the first frame update
-    public List<Item> items;
 
+    public int fireFlowerCount;
+    public int waterFlowerCount; 
     void Start()
     {
-        // Initialize item count text for each item
-        foreach (Item item in items)
+        if(gameManager == null)
         {
-            UpdateItemText(item);
+            gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         }
+        fireFlowerCount = 0;
     }
 
+    private void Awake()
+    {
+        if (gameManager == null)
+        {
+            gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
+        fireFlowerCount = 0;
+    }
+    // Update is called once per frame
     void Update()
     {
-        //
+        if(fireFlowerCount != gameManager.saveData.inventoryData.fireFlowerCount)
+        {
+            fireFlowerCount = gameManager.saveData.inventoryData.fireFlowerCount;
+            TextMeshProUGUI FireFlowerText = GameObject.Find("FirePlanter_text").GetComponent<TextMeshProUGUI>();
+            FireFlowerText.text = fireFlowerCount.ToString();
+        }
+        
     }
 
-    public void AddItem(ItemType itemType, int num)
+    public void addFireFlower(int num)
     {
-        Item item = items.Find(i => i.itemType == itemType);
-        if (item != null)
-        {
-            item.count += num;
-            UpdateItemText(item);
-        }
-        else
-        {
-            Debug.LogWarning($"Item of type {itemType} not found in the inventory.");
-        }
-    }
-
-    public void UseItem(ItemType itemType, int num)
-    {
-        Item item = items.Find(i => i.itemType == itemType);
-        if (item != null)
-        {
-            if (item.count >= num)
-            {
-                item.count -= num;
-                UpdateItemText(item);
-            }
-            else
-            {
-                Debug.Log("Not enough items to use.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"Item of type {itemType} not found in the inventory.");
-        }
-    }
-
-    private void UpdateItemText(Item item)
-    {
-        if (item.itemCountText != null)
-        {
-            item.itemCountText.text = item.count.ToString();
-        }
-        else
-        {
-            Debug.LogWarning($"Text component for {item.itemType} is not assigned.");
-        }
+        gameManager.saveData.inventoryData.fireFlowerCount += num;
     }
 }
