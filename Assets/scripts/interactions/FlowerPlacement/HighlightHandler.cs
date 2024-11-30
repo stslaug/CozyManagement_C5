44fbda6246315e4390b67ecc2937dfe7b13556d0
abcarso <lainalaire@gmail.com>
2024-11-30 15:04:48 -0500

    Fixed highlight bugs

---
+++ Assets/scripts/interactions/FlowerPlacement/HighlightHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightHandler : MonoBehaviour
+{
+    public List<PlacementPoint> placementPoints; // List of all placement points
+    public void Start()
+    {
+        ClearHighlights();
+    }
+
+    // Highlight all valid placement points
+    public void HighlightValidPoints()
+    {
+        //Debug.Log("Highlights added.");
+        foreach (var point in placementPoints)
+        {
+            if (point.IsAvailable())
+            {
+                point.SetHighlight(true);
+            }
+            else
+            {
+                point.SetHighlight(false);
+            }
+        }
+    }
+
+    // Disable all highlights
+    public void ClearHighlights()
+    {
+        foreach (var point in placementPoints)
+        {
+            point.SetHighlight(false);
+        }
+        
+    }
+
+}
+
+
